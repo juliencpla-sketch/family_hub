777107b0b71104c4b585f77c4a5c505b05ddb6f9
julien-pla <julien.c.pla@gmail.com>
2026-01-21 18:13:42 +0100

    navbar/stage 1

--- family_hub_front/src/App.tsx
+++ family_hub_front/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./components/navBar";
 
 function App() {
 	return (
 		<div>
-			<h1 className="font-bold text-4xl">Family Hub</h1>
+			<NavBar />
 		</div>
 	);
 }
